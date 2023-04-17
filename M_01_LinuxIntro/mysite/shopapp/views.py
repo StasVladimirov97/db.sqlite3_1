bbb3714a6d381299ca2a8a7b32453f236ce7044a
Stas Vladimirov <stasel0705@gmail.com>
2023-04-17 21:47:55 +0300

    compete

--- M_01_LinuxIntro/mysite/shopapp/views.py
+++ M_01_LinuxIntro/mysite/shopapp/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from timeit import default_timer
 
 def shop_index(request: HttpRequest):
-    print(request.path)
-    print(request.method)
-    print(request.headers)
-    return HttpResponse('<h1>Hello world!<h1>')
+    products = [
+        ('Laptop', 999),
+        ('Desktop', 1999),
+        ('Smartphone', 2999)
+    ]
+    context ={
+        "time_running": default_timer(),
+        "product" : products,
+    }
+
+    return render(request, 'shopapp/shop-index.html')
 # Create your views here.

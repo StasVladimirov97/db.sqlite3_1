c4ccfbb284cb58162154dd086d5bcd52f41cabd4
Stas Vladimirov <stasel0705@gmail.com>
2023-04-19 20:57:31 +0300

    compete

--- M_01_LinuxIntro/mysite/shopapp/views.py
+++ M_01_LinuxIntro/mysite/shopapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from timeit import default_timer
 
-from .models import Product
+from .models import Product, Order
 def shop_index(request: HttpRequest):
     products = [
         ('Laptop', 999),
@@ -31,3 +31,10 @@ def products_list(request: HttpRequest):
         "products" : Product.objects.all(),
     }
     return render(request, "shopapp/products-list.html", context=context)
+
+
+def orders_list(request: HttpRequest):
+    context = {
+        'orders': Order.objects.select_related("user").prefetch_related("products").all(),
+    }
+    return render(request,"shopapp/orders-list.html", context=context)

2070f34b24f7b576b2c39977dbf0b7c15fa89ce6
Stas Vladimirov <stasel0705@gmail.com>
2023-04-23 21:06:09 +0300

    compete

--- M_01_LinuxIntro/mysite/shopapp/admin_mixins.py
+++ M_01_LinuxIntro/mysite/shopapp/admin_mixins.py
@@ -6,18 +6,19 @@ from django.db.models.options import Options
 
 
 class ExportAsCSVMixin:
-    def export_csv(self, request: HttpRequest, quaryset: QuerySet):
+
+    def export_csv(self, request: HttpRequest, queryset: QuerySet):
         meta: Options = self.model._meta
         field_names = [field.name for field in meta.fields]
 
-        response = HttpResponse(content_type="text/scv")
-        response["Content-Disposition"] = f'attacment; filename = {met}-export.csv'
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = f'attachment; filename = {meta}-export.csv'
 
         csv_writer = csv.writer(response)
 
         csv_writer.writerow(field_names)
 
-        for obj in request:
+        for obj in queryset:
             csv_writer.writerow([getattr(obj, field) for field in field_names])
 
         return response

6f5afb9aa9f6e5fecb1453629d16834ff499f549
Stas Vladimirov <stasel0705@gmail.com>
2023-04-24 22:03:57 +0300

    compete

--- M_01_LinuxIntro/mysite/requestdataapp/middlewares.py
+++ M_01_LinuxIntro/mysite/requestdataapp/middlewares.py
@@ -1,4 +1,7 @@
+from datetime import time
+
 from django.http import HttpRequest
+from django.shortcuts import render
 
 
 def set_useragent_on_request_middleware(get_response):
@@ -19,10 +22,21 @@ class CountRequestsMiddleware:
     def __int__(self, get_response):
         self.get_response = get_response
         self.requests_count = 0
+        self.request_time = {}
         self.responses_count = 0
         self.exceptions_count = 0
 
     def __call__(self, request: HttpRequest):
+        time_delay = 10
+        if not self.request_time:
+            print('This first request after return server, the dictionary empty')
+        else:
+            if (round(time.time()) * 1) - self.request_time['time'] < time_delay \
+                    and self.request_time['ip_address'] == request.META.get('REMOTE.ADDR'):
+                print('less than 10 seconds elapsed for retry request')
+                return render(request, 'requestdataapp/error-request.html')
+        self.request_time = {'time': round(time.time()) * 1, 'ip_address': request.META.get('REMOTE.ADDR')}
+
         self.requests_count += 1
         print("requests count ", self.requests_count)
         response = self.get_response(request)

16197f239b2f093965135b38873e0a4de2b0b72c
Stas Vladimirov <stasel0705@gmail.com>
2023-04-27 21:10:11 +0300

    compete

--- M_01_LinuxIntro/mysite/shopapp/forms.py
+++ M_01_LinuxIntro/mysite/shopapp/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from .models import Product
+from django.contrib.auth.models import Group
+from django.forms import ModelForm
+
 class ProductsForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "name", "price", "description", "discount"
 
 
-
+class GroupForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = ["name"]
 

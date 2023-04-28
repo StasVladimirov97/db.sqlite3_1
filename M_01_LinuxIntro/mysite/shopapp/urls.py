60952ca69da818c92d620ce93a459a3c35edc2c8
Stas Vladimirov <stasel0705@gmail.com>
2023-04-28 21:34:18 +0300

    compete

--- M_01_LinuxIntro/mysite/shopapp/urls.py
+++ M_01_LinuxIntro/mysite/shopapp/urls.py
@@ -1,12 +1,25 @@
 from django.urls import path
-from .views import ShopIndexView, GroupsListView, products_list, orders_list, create_product, ProductDetailsView
+from .views import (ShopIndexView,
+                    GroupsListView,
+                    OrdersList,
+
+                    ProductDetailsView,
+                    ProductsListView,
+                    OrderDetailView,
+                    ProductCreateView,
+                    ProductUpdateView,
+                    ProductDeleteView)
 app_name = 'shopapp'
 
 urlpatterns = [
     path('', ShopIndexView.as_view(), name = 'index'),
     path("groups/", GroupsListView.as_view(), name = "groups_list"),
-    path('products/', products_list, name= 'products_list'),
+    path('products/', ProductsListView.as_view(), name= 'products_list'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/', ProductDetailsView.as_view(), name='products_details'),
-    path('orders/', orders_list, name = 'orders-list'),
-    path('products/create/', create_product, name = 'product-create'),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/archive/', ProductDeleteView.as_view(), name='product_delete'),
+    path('orders/', OrdersList.as_view(), name = 'orders_list'),
+    path('orders/<int:pk>', OrderDetailView.as_view(), name='order_details'),
+
 ]

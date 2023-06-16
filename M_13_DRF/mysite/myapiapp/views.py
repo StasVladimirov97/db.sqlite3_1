0ee5d5c08f5e47ff1dcd8230341d31d7fbdfcbb0
Stas Vladimirov <stasel0705@gmail.com>
2023-06-16 12:46:26 +0300

    Complete

--- M_13_DRF/mysite/myapiapp/views.py
+++ M_13_DRF/mysite/myapiapp/views.py
@@ -1,18 +1,18 @@
 from django.contrib.auth.models import Group
-from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from .serializers import GroupSerializer
-
 
 @api_view()
 def hello_world_view(request: Request) -> Response:
-    return Response({"message": "Hello World!"})
+    return Response({"message": "Hello world!"})
 
 
-class GroupsListView(ListCreateAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+class GroupsListView(APIView):
+
+    def get(self, request: Request) -> Response:
+        groups = Group.objects.all()
+        serialized = GroupSerializer(groups, many=True)
+        return Response({"groups": serialized.data})

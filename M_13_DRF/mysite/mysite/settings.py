92345751af9039d20dd17f6c855035ff6b0fbf38
Stas Vladimirov <stasel0705@gmail.com>
2023-06-16 12:01:30 +0300

    Complete

--- M_13_DRF/mysite/mysite/settings.py
+++ M_13_DRF/mysite/mysite/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'shopapp.apps.ShopappConfig',
     'myauth.apps.MyauthConfig',
+    'myapiapp.apps.MyapiappConfig',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy("myauth:about-me")
 LOGIN_URL = reverse_lazy("myauth:login")
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
